a2f4e6ed91b5bc49f07a40e91f4764e1472aa497
Anastasia <tigra695@hotmail.com>
2022-02-21 21:15:40 +0300

    Fixed by comments

--- js/main.js
+++ js/main.js
@@ -1,20 +1,16 @@
-const GET_RANDOM_NUMBER = function (min, max) {
+const getRandomNumber = (min, max) => {
   if (min >= 0 && min < max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
   const ERROR = new Error(
-    'Введены некорректные данные! Диапазон не должен содержать отрицательных значений и максимальное значение диапазона должно быть больше минимального значения!'
+    "Введены некорректные данные! Диапазон не должен содержать отрицательных значений и максимальное значение диапазона должно быть больше минимального значения!"
   );
   console.log(ERROR.message); // eslint-disable-line no-console
 };
 
-const CHECK_MAX_LENGTH = function (string, maxLength) {
-  if (string.length <= maxLength) {
-    return true;
-  }
-  return false;
-};
-GET_RANDOM_NUMBER();
-CHECK_MAX_LENGTH();
+const checkMaxLength = (string, maxLength) => string.length <= maxLength;
+
+getRandomNumber();
+checkMaxLength();
