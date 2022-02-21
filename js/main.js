f73567da096b049a3d2ace386fc527a383575782
Anastasia <tigra695@hotmail.com>
2022-02-21 19:49:46 +0300

    Linter fixes

--- js/main.js
+++ js/main.js
@@ -1,18 +1,20 @@
-let getRandomNumber = function (min, max) {
+const GET_RANDOM_NUMBER = function (min, max) {
   if (min >= 0 && min < max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
   const ERROR = new Error(
-    "Введены некорректные данные! Диапазон не должен содержать отрицательных значений и максимальное значение диапазона должно быть больше минимального значения!"
+    'Введены некорректные данные! Диапазон не должен содержать отрицательных значений и максимальное значение диапазона должно быть больше минимального значения!'
   );
-  console.log(ERROR.message);
+  console.log(ERROR.message); // eslint-disable-line no-console
 };
 
-let checkMaxLength = function (string, maxLength) {
+const CHECK_MAX_LENGTH = function (string, maxLength) {
   if (string.length <= maxLength) {
     return true;
   }
   return false;
 };
+GET_RANDOM_NUMBER();
+CHECK_MAX_LENGTH();
